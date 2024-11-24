081fbea7c7d16a9192fad0b7d0a753d5b7740e42
João Vitor Ferreira <joaovitorferreiralima01@gmail.com>
2024-11-24 17:59:55 -0300

    sera que esse cu vai??

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('collaborator-btn').addEventListener('click', function() {
+    const collaboratorForm = document.getElementById('collaborator-form');
+    collaboratorForm.classList.toggle('hidden');
+});
